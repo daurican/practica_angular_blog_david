8985f8fd838375560c69c404627f49d20a1ddba0
David Melián Godoy <dav.mantodea@gmail.com>
2023-11-11 21:37:50 +0100

    Se define el servicio Post

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { IPost } from '../interfaces/ipost.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  private arrPosts: IPost[];
+
+
+  constructor() {
+    this.arrPosts = [
+      {
+        titulo: '',
+        texto: '',
+        autor: '',
+        imagen: '',
+        fecha: new Date(),
+        categoria: ''
+      }
+    ]
+  }
+
+  createPost(post: IPost) {
+    this.arrPosts.push(post);
+  }
+
+  getAllPosts(): IPost[] {
+    return this.arrPosts;
+  }
+
+  getByCategoria(categoria: string): IPost[] {
+    const categoriaFiltrada: IPost[] = [];
+
+    for (let categoriaElegida of this.arrPosts) {
+      if (categoriaElegida.categoria === categoria) {
+        categoriaFiltrada.push(categoriaElegida);
+      }
+    }
+    return categoriaFiltrada;
+  }
+
+
 }
