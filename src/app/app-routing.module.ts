265ef0f76fb0f7dbf840f68e3a3d1881846af5ae
David Melián Godoy <dav.mantodea@gmail.com>
2023-11-11 20:37:32 +0100

    Se crean ficheros de interfaz, servicio y los componentes ListaPosts y Formulario. Se define la interfaz y se establecen las rutas necesarias en app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaPostsComponent } from './components/lista-posts/lista-posts.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/posts' },
+  { path: 'posts', component: ListaPostsComponent },
+  { path: 'new', component: FormularioComponent },
+  { path: '**', redirectTo: '/posts' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaPostsComponent } from './components/lista-posts/lista-posts.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaPostsComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
